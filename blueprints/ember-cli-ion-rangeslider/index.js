957e536cd94dc122d225b8d26987b255381c3995
Alberto Cantú Gómez <betocantu93@gmail.com>
2018-01-11 15:35:11 -0600

    updated blueprint for use npm instead of bower and also made index.js fastboot and engines compatability

--- blueprints/ember-cli-ion-rangeslider/index.js
+++ blueprints/ember-cli-ion-rangeslider/index.js
@@ -1,8 +1,9 @@
 module.exports = {
   description: 'Sets up required ember-cli-ion-rangeslider dependencies',
-  normalizeEntityName: function(){}, 
+  name: 'ember-cli-ion-rangeslider',
+  normalizeEntityName: function(){},
 
   afterInstall: function(){
-    return this.addBowerPackageToProject('ionrangeslider');
+    return this.addPackageToProject('ion-rangeslider');
   }
 };
